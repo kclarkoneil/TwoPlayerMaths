bcf461d2431be28b1648fc105c60fcc222c8b37d
HighGroundLover <kclarkoneil@yahoo.ca>
2018-08-06 18:29:25 -0700

    Initial Commit

---
+++ TwoPlayersMaths/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwoPlayersMaths
+//
+//  Created by Kit Clark-O'Neil on 2018-08-06.
+//  Copyright © 2018 Kit Clark-O'Neil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwoPlayersMaths/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TwoPlayersMaths
+//
+//  Created by Kit Clark-O'Neil on 2018-08-06.
+//  Copyright © 2018 Kit Clark-O'Neil. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
